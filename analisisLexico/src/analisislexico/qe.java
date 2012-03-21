2b337dd170bee10ce4f5319e5169ca606a66f56f
Fernando Lopez <valefurx@hotmail.com>
2012-03-21 13:41:11 -0700

    Comentarios de qe

--- analisisLexico/src/analisislexico/qe.java
+++ analisisLexico/src/analisislexico/qe.java
@@ -2,18 +2,18 @@ package analisislexico;
 
 import javax.swing.JOptionPane;
 
-/*                                 *| 
+/*                                  *|
  |*                                 *|
  |*             By Wolf             *|
  |*                                 *|
  |*                                 */
 
-public class qe {
+public class qe {// NO terminado
     public static void analisis(char cadena[],int indice){
         String error="";
-        if(indice>5){
+        if(indice>5){// controla que el mensaje solo se muestre si el indice se a movido almenos 5 veces
             for(int i= indice-5;i<indice;i++){
-                error = error+cadena[i];
+                error = error+cadena[i]; // Se concatena la cadena hasta donde se a leido
             }
             JOptionPane.showMessageDialog(null, "Error lexico cerca de '"+error+"'");
         }
