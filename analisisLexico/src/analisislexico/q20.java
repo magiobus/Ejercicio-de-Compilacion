e6c338a01e93c23051a7632415b483e0d924300e
aalbertobus <aalbertobus@gmail.com>
2012-03-21 12:30:58 -0700

    Corregido error vim en q20

--- analisisLexico/src/analisislexico/q20.java
+++ analisisLexico/src/analisislexico/q20.java
@@ -15,18 +15,10 @@ public class q20 {
         if(indice < cadena.length-1){ // Revisa si no esta en el ultimo caracter 
             indice++;
             q0.analisis(cadena, indice);
-<<<<<<< HEAD
-            
-          } else { // Si esta en el ultimo caracter, termina el programa 
-              System.out.println("fin programa");
-              System.exit(0);
-            }
-  }
-=======
-        }else { // Si esta en el ultimo caracter, termina el programa 
+
+        } else { // Si esta en el ultimo caracter, termina el programa 
             System.out.println("fin programa");
             System.exit(0);
-        }        
+          }
     }
->>>>>>> f07f3da0ccf4896ce23cc1061d5e6878bf88baa1
 }
