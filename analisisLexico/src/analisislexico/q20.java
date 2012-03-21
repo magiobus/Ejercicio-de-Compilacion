f07f3da0ccf4896ce23cc1061d5e6878bf88baa1
Fernando Lopez <valefurx@hotmail.com>
2012-03-21 12:14:12 -0700

    correccion de identacion

--- analisisLexico/src/analisislexico/q20.java
+++ analisisLexico/src/analisislexico/q20.java
@@ -9,30 +9,15 @@ package analisislexico;
  * @author Macbook
  */
 public class q20 {
-  public static void analisis(char cadena[],int indice){
+    public static void analisis(char cadena[],int indice){
         System.out.println("q20: indice ="+indice);
-          if(indice < cadena.length-1){ // Revisa si no esta en el ultimo caracter 
+
+        if(indice < cadena.length-1){ // Revisa si no esta en el ultimo caracter 
             indice++;
             q0.analisis(cadena, indice);
-            
-          } else { // Si esta en el ultimo caracter, termina el programa 
-              System.out.println("fin programa");
-              System.exit(0);
-            }
-          
-        
-        
-        
-        
-        
-        
-        
-        
-        
-  }
-    
-    
-    
-    
-    
+        }else { // Si esta en el ultimo caracter, termina el programa 
+            System.out.println("fin programa");
+            System.exit(0);
+        }        
+    }
 }
