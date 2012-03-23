179cd327bec9ba66429a3f915577030575f11622
aalbertobus <aalbertobus@gmail.com>
2012-03-23 11:46:05 -0700

    q29 Comentarios Actualizados ¬¬

--- analisisLexico/src/analisislexico/q29.java
+++ analisisLexico/src/analisislexico/q29.java
@@ -9,43 +9,36 @@ package analisislexico;
  * @author Macbook
  */
 public class q29 {
-    public static void analisis(char cadena[],int indice){
-        System.out.println("q29: indice ="+indice);     
-        
-        if(indice-1 == cadena.length-1){ // ¬()si no hay nada .
+
+    public static void analisis(char cadena[], int indice) {
+        System.out.println("q29: indice =" + indice);
+        boolean estado = false;
+
+        if (indice - 1 == cadena.length - 1) { // ¬()si no hay nada .
+            indice = indice - 1;
+            estado = true;
             qe.analisis(cadena, indice);
         }
-        
-        if(cadena[indice] != '¬'){ // si es ¬(mas caracteres diferentes a ¬) error
+
+        if (cadena[indice] != '¬') { // si es ¬(mas caracteres diferentes a ¬) error
             qe.analisis(cadena, indice);
         }
-       
-        while(indice < cadena.length-1){
-            if(cadena[indice] == '¬'){ // si es ¬(¬) y hay mas caracteres
+
+
+        while (indice < cadena.length - 1) {
+            if (cadena[indice] == '¬') { // si es ¬(¬) y hay mas caracteres
                 System.out.println("¬¬...");
                 indice++;
                 q0.analisis(cadena, indice);
             }
         }
-        
-        while(indice == cadena.length-1){
-            if(cadena[indice] == '¬'){ // Si es ¬(¬) y no hay mas caracteres
+
+        while (indice == cadena.length - 1 && estado == false) {
+            if (cadena[indice] == '¬') { // Si es ¬(¬) y no hay mas caracteres
                 System.out.println("¬¬ Fin");
                 System.out.println("fin de programa");
                 System.exit(0);
             }
         }
-        
-        
     }
 }
-        
-        
-        
-        
-       
-            
-          
-    
-
-
