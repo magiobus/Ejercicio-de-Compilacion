5a005b5d9c03a11c7cee084dbdf2a243fe23cfa0
Fernando Lopez <valefurx@hotmail.com>
2012-04-14 16:41:14 -0600

    arreglado un error en q33 al procesar cadenas que no son identificadores

--- analisisLexico/src/analisislexico/q33.java
+++ analisisLexico/src/analisislexico/q33.java
@@ -12,22 +12,29 @@ import General.TablaDeSimbolos;
  */
 public class q33 {
     public static void analisis(char cadena[],int indice){
-        System.out.println("q26: indice ="+indice);
+        System.out.println("q33: indice ="+indice);
         String valorLexema="";
         
         if(indice < cadena.length-1){ // Revisa si no esta en el ultimo caracter 
-            while(MapaDeCaracteres.isValid(cadena[indice])){
-                indice++;
-                if(cadena[indice]=='"'){
-                    for(int c = TablaDeSimbolos.inicioLexema; c<indice+1;c++){ //lee el valor del lexema procesado y lo asigna a la variable 
+            if(cadena[indice]=='"'){
+                for(int c = TablaDeSimbolos.inicioLexema; c<indice+1;c++){ //lee el valor del lexema procesado y lo asigna a la variable 
                         valorLexema=valorLexema+cadena[c];
                     }
                     AnalisisLexico.resultadoAnalisis+="<cadena,"+valorLexema+">";
                     q0.analisis(cadena, indice);
-                }
             }
+            else if(MapaDeCaracteres.isValid(cadena[indice])){
+                q33.analisis(cadena, indice+1);
+            }
+                
         } 
-        else { // Si esta en el ultimo caracter, termina el programa 
+        else { // Si esta en el ultimo caracter, termina el programa
+            if(cadena[indice]=='"'){
+                for(int c = TablaDeSimbolos.inicioLexema; c<indice+1;c++){ //lee el valor del lexema procesado y lo asigna a la variable 
+                            valorLexema=valorLexema+cadena[c];
+                }
+                AnalisisLexico.resultadoAnalisis+="<cadena,"+valorLexema+">";
+            }
             qf.end();
         }
     }
